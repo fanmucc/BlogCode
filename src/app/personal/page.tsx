1a13da8da70145bc741525d71e4637daa6033151
严文强 <fanmucc@yeah.net>
2024-01-09 00:21:54 +0800

    Feat: 基础组件及基础页面展示

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,13 +1,13 @@
-export default function Home() {
+export default function Personal() {
 	return (
 		<main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
 			<div className='text-center'>
 				<p className='text-8xl font-semibold text-indigo-600'>404</p>
 				<h1 className='mt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
-					文库正在建设中
+					个人中心正在建设中
 				</h1>
 				<p className='mt-10 text-base leading-7 text-gray-600'>
-					对不起，文库正在建设中，您可以浏览其他开发者所创作的优秀文章
+					对不起，个人中心正在建设中，您可以浏览其他开发者所创作的优秀文章
 				</p>
 				{/* <div className='mt-10 flex items-center justify-center gap-x-6'>
 					<a
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+export default () => {
+	return (
+		<div className='position: fixed bottom-10 left-0 right-0 flex flex-1 items-center justify-center w-100%'>
+			个人网站备案号:
+			<a
+				href='https://beian.miit.gov.cn/'
+				target='_blank'
+				className='ml-1 text-[#f59e0b]'
+			>
+				豫ICP备2023039354号-1
+			</a>
+		</div>
+	);
+};
