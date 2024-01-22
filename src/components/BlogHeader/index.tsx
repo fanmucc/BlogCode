fe5cc47c9f1bb224a6455712ea9353b9d13965e4
严文强 <fanmucc@yeah.net>
2024-01-22 23:59:55 +0800

    Feat: header滚动背景色

--- src/components/BlogHeader/index.tsx
+++ src/components/BlogHeader/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import {
 	Navbar,
@@ -23,8 +24,23 @@ import IconFont from "@/components/BusComponents/IconFont";
 
 const BlogHeader = () => {
 	const pathname = usePathname();
+	const [scroll, setScroll] = useState(false);
+
+	// 监听页面滚动
+	useEffect(() => {
+		const handleScroll = () => {
+			let scrollDistance = window.scrollY || document.documentElement.scrollTop;
+			setScroll(scrollDistance > 80);
+			console.log(scrollDistance);
+		};
+		document.addEventListener("scroll", handleScroll);
+	}, []);
 	return (
-		<Navbar className='h-[60px] bg-blog-bg-color'>
+		<Navbar
+			className={`h-[60px] bg-blog-bg-color ${
+				scroll ? "bg-blog-card-bg outline-1 outline-bg-blog-card-bg" : ""
+			}`}
+		>
 			<NavbarBrand>
 				{/* <Logo /> */}
 				<div className='group h-[35px] w-[70px] cursor-pointer'>
@@ -33,11 +49,11 @@ const BlogHeader = () => {
 						href='/'
 					>
 						Fan'mu
-						<span className='absolute left-0 top-0 h-[35px] w-[70px] bg-blog-text-primary/100 flex items-center justify-center hidden group-hover:flex'>
+						<span className='hidden absolute left-0 top-0 h-[35px] w-[70px] bg-blog-text-primary/100 flex items-center justify-center group-hover:flex'>
 							<IconFont
 								type='icon-[material-symbols-light--home-rounded]'
 								size={28}
-								className='text-blog-bg-color'
+								className='text-blog-card-bg'
 							/>
 						</span>
 					</Link>
